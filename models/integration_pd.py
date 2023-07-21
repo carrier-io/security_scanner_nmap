305ed79976a0e423e0b7a2ffac617d75a485e301
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-07-21 11:01:49 +0500

    Validation for ports were added

--- models/integration_pd.py
+++ models/integration_pd.py
@@ -1,5 +1,7 @@
+import re
 from typing import Optional
 from pydantic import BaseModel
+from pydantic.class_validators import validator
 from pylon.core.tools import log
 
 
@@ -17,3 +19,23 @@ class IntegrationModel(BaseModel):
         except Exception as e:
             log.exception(e)
             return False
+
+    @validator('include_ports', 'exclude_ports')
+    def validate_ports(cls, value):
+        # Validate if the ports value is in the correct format
+        # The correct format can be either "start-end", "port", or a comma-separated list of "port" or "start-end"
+        if not re.match(r'^(\d+-\d+|\d+)(,(\d+-\d+|\d+))*$', value):
+            raise ValueError("Use formats 'start-end', 'port', or 'port,port,...'.")
+
+        # Validate each port or port-range within the range 0-65535
+        for port_or_range in value.split(','):
+            if '-' in port_or_range:
+                start, end = map(int, port_or_range.split('-'))
+                if not (0 <= start <= 65535 and 0 <= end <= 65535):
+                    raise ValueError("Invalid port range. Ports must be between 0 and 65535.")
+            else:
+                port = int(port_or_range)
+                if not (0 <= port <= 65535):
+                    raise ValueError("Invalid port. Ports must be between 0 and 65535.")
+
+        return value
